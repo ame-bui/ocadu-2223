560d98a5e3f4bf6ef5896156f988e666b6571aad
ame-bui <66383380+ame-bui@users.noreply.github.com>
2022-11-04 12:04:10 -0400

    update 2.0

--- 2-timemachine/sketch.js
+++ 2-timemachine/sketch.js
@@ -9,11 +9,20 @@ $(function(){
     $myDiv.show('slow');
     $navDiv.hide('slow');
   });
+
+  $("#sticky-nav-button").on('click', function (){
+    $navDiv.toggle();
+  })
 });
 
-$("button").click(function() {
+$("#start-button").click(function() {
   $('.container').animate({
       scrollTop: $("#the-book").offset().top},
       'slow');
 });
+
+
+
+
+// https://css-tricks.com/an-explanation-of-how-the-intersection-observer-watches/
 
