b14e740448a598a86b467c462542a6146ecba90b
ame-bui <66383380+ame-bui@users.noreply.github.com>
2022-12-06 11:17:37 -0500

    demo-2

--- 2-timemachine/highlight.js
+++ 2-timemachine/highlight.js
@@ -9,9 +9,9 @@ function setup(){
 
 function draw(){
     if(mouseIsPressed){
-        stroke(255);
+        stroke(random(0,255),random(0,255),245);
+        strokeWeight(2);
         line(pmouseX, pmouseY, mouseX, mouseY);
         canvas.style('z-index','100');
-
     }
 }
